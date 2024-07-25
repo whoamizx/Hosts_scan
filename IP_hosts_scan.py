469b8d8b5fed0084269d7ec7283dfe289ab4ae7c
whoamizx <544722646@qq.com>
2024-07-25 22:38:36 +0800

    改成可以扫描前缀的工具

--- IP_hosts_scan.py
+++ IP_hosts_scan.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: UTF-8 -*-
 #Author:R3start
 #这是一个用于IP和域名碰撞匹配访问的小工具
@@ -16,22 +15,24 @@ for iplist in open("ip.txt"):
     #读取host地址
     http_s = ['http://','https://']
     for h in http_s :
-        for hostlist in open("host.txt",'r'):
-            host = hostlist.strip('\n')
-            headers = {'Host':host,'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
-            try:
-                r = requests.session()
-                requests.packages.urllib3.disable_warnings()
-                rhost = r.get(h + ip,verify=False,headers=headers,timeout=5)
-                rhost.encoding='utf-8'
-                title = re.search('<title>(.*)</title>', rhost.text).group(1) #获取标题
-                info = '%s -- %s 协议：%s 数据包大小：%d 标题：%s' % (ip,host,h,len(rhost.text),title)
-                lists.append(info)
-                files.write(info + "\n")
-                print(info)
-            except Exception :
-                error = ip + " --- " + host + " --- 访问失败！~"
-                print(error)
+        for d in open("top-3k-domain.txt",'r'):
+            d=d.strip('\n') 
+            for hostlist in open("host.txt",'r'):
+                host = d+"."+hostlist.strip('\n')
+                headers = {'Host':host,'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36'}
+                try:
+                    r = requests.session()
+                    requests.packages.urllib3.disable_warnings()
+                    rhost = r.get(h + ip,verify=False,headers=headers,timeout=5)
+                    rhost.encoding='utf-8'
+                    title = re.search('<title>(.*)</title>', rhost.text).group(1) #获取标题
+                    info = '%s -- %s 协议:%s 数据包大小:%d 标题:%s' % (ip,host,h,len(rhost.text),title)
+                    lists.append(info)
+                    files.write(info + "\n")
+                    print(info)
+                except Exception :
+                    error = ip + " --- " + host + " --- 访问失败！~"
+                    print(error)
 print("====================================匹 配 成 功 的 列 表====================================")
 for i in lists:
     print(i)
